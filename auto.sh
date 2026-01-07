84d96fd6321b29477f77c24635f6105e22489c4f
miewoiorio99 <miewoiorio99@tutanota.com>
2026-01-07 09:37:15 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,7 +5,7 @@ set -e
 LIST_URL="https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_BASE64.txt"
 RAW_FILE="/tmp/v2ray_raw.txt"
 DECODED_FILE="/tmp/v2ray_decoded.txt"
-CONFIG="/usr/local/etc/v2ray/config.json"
+CONFIG="$HOME/v2ray/config.json"
 
 echo "[+] Downloading Base64 list..."
 curl -s "$LIST_URL" -o "$RAW_FILE"
